3cf9d948fc0a77fb9abd38d158f9a66a752f0657
wserafin <wserafin@redhat.com>
2017-10-23 10:04:03 -0400

    Update image.php
    change1

--- image.php
+++ image.php
@@ -3,7 +3,7 @@
 $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 
-$deployment = getenv("COLOR");
+$deployment = 'green';
 
 if ( $deployment == 'blue') {
   $color = imagecolorallocate($im, 0, 0, 255);
